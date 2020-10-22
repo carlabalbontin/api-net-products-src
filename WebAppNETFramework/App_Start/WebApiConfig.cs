10f0f8ae2263c4330cb5864054eb802853d930e0
Carla Balbontín Tanhnuz <cbalbontin@inacap.cl>
2020-10-22 16:20:13 -0300

    First commit

---
+++ WebAppNETFramework/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAppNETFramework
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            var cors = new EnableCorsAttribute(
+                              "*", // allowed websites
+                              "*", // allowed headers
+                              "*"); // allowed methods
+            config.EnableCors(cors);
+
+            // JSON camel case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
